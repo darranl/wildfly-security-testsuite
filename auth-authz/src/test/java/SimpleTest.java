41c575a4a76f52c34f9b0d6bfcc88808596d0b23
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-05-01 12:43:11 +0100

    Add a new test module for testing authentication scenarios.
    This will also include authorization tied to authentication.

---
+++ auth-authz/src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void runTest() {
+
+    }
+
+}
