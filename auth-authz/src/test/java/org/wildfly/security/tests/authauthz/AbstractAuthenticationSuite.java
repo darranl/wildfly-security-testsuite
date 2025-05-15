d6568a3e28e83d0e976a7c427ee21e66eb136cf2
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-05-15 14:55:35 +0100

    Initialise a SecurityRealm backed by a Map

--- auth-authz/src/test/java/org/wildfly/security/tests/authauthz/AbstractAuthenticationSuite.java
+++ auth-authz/src/test/java/org/wildfly/security/tests/authauthz/AbstractAuthenticationSuite.java
@@ -5,8 +5,20 @@
 
 package org.wildfly.security.tests.authauthz;
 
+import java.security.Provider;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.junit.platform.suite.api.AfterSuite;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.wildfly.security.auth.permission.LoginPermission;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.auth.server.SecurityRealm;
+import org.wildfly.security.permission.PermissionVerifier;
 
 /**
  * Base definition of the {@code Suite} of tests that will be used to run the authentication tests
@@ -20,7 +32,18 @@ import org.junit.platform.suite.api.Suite;
         DynamicAuthPermutationsSuiteRunner.class})
 public abstract class AbstractAuthenticationSuite {
 
+    // TODO - This will become the providers needed for testing.
+    static final Supplier<Provider[]> TEST_PROVIDERS = Security::getProviders;
+
+    private static final String REALM_NAME = "TestRealm";
+
     private static String mode = "";
+
+    @AfterSuite
+    public static void endSuite() {
+        //TODO - Can we handle all clean up on our own?
+        System.out.printf("endSuite() called for mode='%s'\n", mode);
+    }
 
     static void setMode(final String mode) {
         AbstractAuthenticationSuite.mode = mode;
@@ -29,4 +52,28 @@ public abstract class AbstractAuthenticationSuite {
     static String getMode() {
         return mode;
     }
+
+    static SecurityDomain createSecurityDomain(final Supplier<SecurityRealm> securityRealmSupplier) {
+        final SecurityDomain.Builder domainBuilder = SecurityDomain.builder();
+        domainBuilder.addRealm(REALM_NAME, securityRealmSupplier.get()).build();
+        domainBuilder.setDefaultRealmName(REALM_NAME);
+
+        // Just grant login permission for now.
+        domainBuilder.setPermissionMapper(
+                (p, r) -> PermissionVerifier.from(new LoginPermission()));
+
+        return domainBuilder.build();
+    }
+
+    static Stream<IdentityDefinition> obtainTestIdentities() {
+        List<IdentityDefinition> identities = new ArrayList<>(10);
+        for (int i = 0 ; i < 10 ; i++) {
+            identities.add(new IdentityDefinition(String.format("user%d", i),
+                    String.format("password%d", i)));
+        }
+
+        return identities.stream();
+    }
+
+    record IdentityDefinition(String username, String password) {}
 }
--- auth-authz/src/test/java/org/wildfly/security/tests/authauthz/MapSecurityRealmTest.java
+++ auth-authz/src/test/java/org/wildfly/security/tests/authauthz/MapSecurityRealmTest.java
@@ -5,7 +5,24 @@
 
 package org.wildfly.security.tests.authauthz;
 
+import java.security.spec.InvalidKeySpecException;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.platform.suite.api.BeforeSuite;
+import org.wildfly.security.auth.realm.SimpleMapBackedSecurityRealm;
+import org.wildfly.security.auth.realm.SimpleRealmEntry;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.auth.server.SecurityRealm;
+import org.wildfly.security.authz.Attributes;
+import org.wildfly.security.credential.Credential;
+import org.wildfly.security.credential.PasswordCredential;
+import org.wildfly.security.password.Password;
+import org.wildfly.security.password.PasswordFactory;
+import org.wildfly.security.password.interfaces.ClearPassword;
+import org.wildfly.security.password.spec.ClearPasswordSpec;
+
 
 /**
  * A {@code Suite} instance for testing against a {@code SecurityRealm} backed by a {@code Map}
@@ -14,9 +31,50 @@ import org.junit.platform.suite.api.BeforeSuite;
  */
 public class MapSecurityRealmTest extends AbstractAuthenticationSuite {
 
+    private static PasswordFactory passwordFactory;
     @BeforeSuite
-    public static void setup() {
+    public static void setup() throws Exception {
+        // Step 0 - Pre-Initialisation
+        passwordFactory = PasswordFactory.getInstance(ClearPassword.ALGORITHM_CLEAR, TEST_PROVIDERS);
+
         setMode("MAP");
+        // Step 1 - Configured additional required servers.
+        // (Not needed for Map as in-memory)
+
+        // Step 2 - Initialise the SecurityDomain
+        SecurityDomain securityDomain = createSecurityDomain(MapSecurityRealmTest::createSecurityRealm);
+
+        // Step 3 - Initialise the HTTP process(es)
+        // Can we do path based?
+
+        // Step 4 - Initialise the Remoting Connectors
+        // Make authentication swappable or should a single connector support all mechs?
+
+        // Can we get all modes available for a single server process?
+    }
+
+    static SecurityRealm createSecurityRealm() {
+        SimpleMapBackedSecurityRealm realm = new SimpleMapBackedSecurityRealm(TEST_PROVIDERS);
+
+        realm.setIdentityMap(obtainTestIdentities().collect(
+                Collectors.toMap(IdentityDefinition::username,
+                        i -> {
+                            List<Credential> credentials =
+                                    Collections.singletonList(new PasswordCredential(toPassword(i.password())));
+
+                            return new SimpleRealmEntry(credentials, Attributes.EMPTY);
+                        }
+        )));
+
+        return realm;
+    }
+
+    static Password toPassword(final String password) {
+        try {
+            return passwordFactory.generatePassword(new ClearPasswordSpec(password.toCharArray()));
+        } catch (InvalidKeySpecException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
 }
