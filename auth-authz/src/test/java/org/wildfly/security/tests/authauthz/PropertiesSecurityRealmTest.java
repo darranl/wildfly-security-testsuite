244b0ab3ca76bbb2721aa504d82123de437b7e23
Ondrej Kotek <okotek@redhat.com>
2025-05-29 14:59:54 +0200

    Use simple plain realm and mechanism in PropertiesSecurityRealmTest for the beginning

--- auth-authz/src/test/java/org/wildfly/security/tests/authauthz/PropertiesSecurityRealmTest.java
+++ auth-authz/src/test/java/org/wildfly/security/tests/authauthz/PropertiesSecurityRealmTest.java
@@ -5,7 +5,15 @@
 
 package org.wildfly.security.tests.authauthz;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.platform.suite.api.BeforeSuite;
+import org.wildfly.security.auth.realm.LegacyPropertiesSecurityRealm;
+import org.wildfly.security.auth.server.SecurityRealm;
+import static org.wildfly.security.tests.authauthz.AbstractAuthenticationSuite.createTestServer;
 
 /**
  * A {@code Suite} instance for testing against a {@code SecurityRealm} backed by a properties file.
@@ -15,8 +23,31 @@ import org.junit.platform.suite.api.BeforeSuite;
 public class PropertiesSecurityRealmTest extends AbstractAuthenticationSuite {
 
     @BeforeSuite
-    public static void setup() {
+    public static void setup() throws Exception {
+        registerProvider();
         setMode("PROPERTIES");
+
+        Set<String> supportedMechanims = new HashSet<>();
+        Collections.addAll(supportedMechanims, "PLAIN");
+
+        createTestServer(PropertiesSecurityRealmTest::createSecurityRealm,
+                Collections.unmodifiableSet(supportedMechanims));
     }
 
+    static SecurityRealm createSecurityRealm() {
+        StringBuilder identitiesString = new StringBuilder();
+        obtainTestIdentities().forEach(identity -> {
+            identitiesString.append(String.format("%s=%s\n", identity.username(), identity.password()));
+        });
+        try {
+            LegacyPropertiesSecurityRealm realm = LegacyPropertiesSecurityRealm.builder()
+                    .setPlainText(true)
+                    .setUsersStream(new ByteArrayInputStream(identitiesString.toString().getBytes()))
+                    .build();
+
+            return realm;
+        } catch (IOException ex) {
+            throw new IllegalStateException("Unable to initialize " + PropertiesSecurityRealmTest.class.getName(), ex);
+        }
+    }
 }
