f2bd743d862b0b6b5f18eb1cfa53dd8dcf46e450
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-06-13 18:18:22 +0100

    Issue#2 Bring in the arquillian dependencies.

---
+++ integration/auth-authz/src/test/java/org/wildfly/security/tests/integration/authauthz/HelloWorldTest.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.wildfly.security.tests.integration.authauthz;
+
+/**
+ *
+ */
+public class HelloWorldTest {
+
+}
